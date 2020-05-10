e162500f0ca39ec2c80936c837ebe76c3df5e3e4
Mike Zhu <zhuyuan2706@gmail.com>
2020-05-10 16:57:59 +0800

    screen share client update

--- rpc/screenshareclient.cpp
+++ rpc/screenshareclient.cpp
@@ -7,8 +7,8 @@ ScreenShareClient::ScreenShareClient(QObject *parent) : QObject(parent)
 {
     socket = new QTcpSocket();
     connect(socket, SIGNAL(readyRead()), this, SLOT(readData()));
-    socket->setReadBufferSize(90000);
-    socket->connectToHost("0.0.0.0", 8910);
+    socket->setReadBufferSize(9000000);
+    socket->connectToHost("10.211.55.3", 8910);
     socket->connected();
 }
 
@@ -29,8 +29,9 @@ void ScreenShareClient::readData()
     QTime time;
     time.start();
     quint32 bytesLen = socket->bytesAvailable();
+    quint32 readLen = 0;
     qDebug("readData: %d", bytesLen);
-    if (bytesLen < 1000)
+    if (bytesLen < 100)
     {
         qDebug() << socket->readAll();
     } else
@@ -41,8 +42,20 @@ void ScreenShareClient::readData()
                 QDataStream in(socket);
                 in >> len;
                 qDebug() << "datalen:" << len;
+                if (bytesLen - readLen > len + 1000)
+                {
+                    socket->read(len - data_len);
+                    readLen += len - data_len;
+                    array.clear();
+                    array.squeeze();
+                    array.reserve(data_len);
+                    len = 0xffffffff;
+                    data_len = 0;
+                    continue;
+                }
             }
             array.append(socket->read(len - data_len));
+            readLen += len - data_len;
             data_len = array.size();
 
             if (data_len == len)
@@ -51,7 +64,7 @@ void ScreenShareClient::readData()
                 pixmap.loadFromData(array, "JPG");
                 array.clear();
                 array.squeeze();
-                array.reserve(1);
+                array.reserve(data_len);
                 len = 0xffffffff;
                 data_len = 0;
                 qDebug() << "Decode time:" << time.elapsed();
